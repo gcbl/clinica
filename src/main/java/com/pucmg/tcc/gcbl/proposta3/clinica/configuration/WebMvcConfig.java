402dd502dbf5a01a2ad06a5e6157ebe6ec2994af
Gil <eu@anywhere.com>
2018-04-25 13:53:27 -0300

    addResourceHandlers

--- src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/configuration/WebMvcConfig.java
+++ src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/configuration/WebMvcConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
@@ -32,6 +33,13 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
     public BCryptPasswordEncoder passwordEncoder() {
         BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
         return bCryptPasswordEncoder;
+    }
+    
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+          .addResourceHandler("/resources/static/**")
+          .addResourceLocations("/resources/static/"); 
     }
 
 }
