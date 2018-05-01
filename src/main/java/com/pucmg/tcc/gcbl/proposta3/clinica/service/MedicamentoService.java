962f69d1c8f9b18ff55f49b4d9372fbda3d216a2
Gil <Gil@somewhere>
2018-05-01 01:01:49 -0300

    Versao inicial

---
+++ src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/service/MedicamentoService.java
@@ -0,0 +1,19 @@
+package com.pucmg.tcc.gcbl.proposta3.clinica.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.pucmg.tcc.gcbl.proposta3.clinica.model.Medicamento;
+import com.pucmg.tcc.gcbl.proposta3.clinica.repository.MedicamentoRepository;
+
+//@Service("userService")
+@Service
+public class MedicamentoService{
+
+	@Autowired
+	protected MedicamentoRepository medicamentoRepository;
+	
+    public void salvarMedicamento(Medicamento medicamento) {
+    	medicamentoRepository.save(medicamento);
+    }
+
+}
