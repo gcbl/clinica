7bf3887eada728a9a11e1baa6d4d8349827367f1
Gil <Gil@somewhere>
2018-05-01 00:15:46 -0300

    .

--- src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/ClinicaApplication.java
+++ src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/ClinicaApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ClinicaApplication {
 
 	public static void main(String[] args) {
-	    System.out.println("vai!");
 		SpringApplication.run(ClinicaApplication.class, args);
 	}
 }
