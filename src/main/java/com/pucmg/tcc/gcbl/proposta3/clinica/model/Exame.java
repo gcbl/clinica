02f3f7cf7a1a91d643353821d6a0a5924aa916a2
Gil <Gil@somewhere>
2018-05-01 19:23:59 -0300

    .

--- src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/model/Exame.java
+++ src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/model/Exame.java
@@ -15,11 +15,18 @@ import lombok.EqualsAndHashCode;
 @Table(name = "SC_EXAME")
 public class Exame extends BaseEntity {
 
+	// http://www.unimedpelotas.com.br/downloads/tiss/download.php?arquivo=ExamesLaboratoriais.xls
 
+	/**
+	 * Nomeclatura usual
+	 */
     @Column(name = "NOME")
     @NotEmpty(message = "*Por favor informe o nome do exame")
     private String nome;
 
+    /**
+     * Descricao Tabela TUSS
+     */
     @Column(name = "DESCRICAO")
     @NotEmpty(message = "*Por favor informe a descricao do exame")
     private String descricao;
