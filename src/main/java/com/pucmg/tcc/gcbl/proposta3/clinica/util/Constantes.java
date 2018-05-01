4c3ce8c86d146c7e36ec647d0188e521b66cf657
Gil <Gil@somewhere>
2018-05-01 01:00:54 -0300

    .

--- src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/util/Constantes.java
+++ src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/util/Constantes.java
@@ -1,12 +1,7 @@
 package com.pucmg.tcc.gcbl.proposta3.clinica.util;
 
-import java.util.Date;
-
 public class Constantes {
     
-    // Processo
     public static final String VERSAO_DO_SISTEMA = "201804181819";  // Versao do sistema
-    
-    
 	
 }
