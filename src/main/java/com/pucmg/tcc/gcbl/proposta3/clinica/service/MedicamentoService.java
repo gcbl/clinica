60d58c68b3c78c1dee375b7e216aa8cef1d891fe
Gil <Gil@somewhere>
2018-05-01 19:51:35 -0300

    .

--- src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/service/MedicamentoService.java
+++ src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/service/MedicamentoService.java
@@ -1,7 +1,10 @@
 package com.pucmg.tcc.gcbl.proposta3.clinica.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import com.pucmg.tcc.gcbl.proposta3.clinica.model.Medicamento;
 import com.pucmg.tcc.gcbl.proposta3.clinica.repository.MedicamentoRepository;
 
@@ -12,8 +15,13 @@ public class MedicamentoService{
 	@Autowired
 	protected MedicamentoRepository medicamentoRepository;
 	
+	
     public void salvarMedicamento(Medicamento medicamento) {
     	medicamentoRepository.save(medicamento);
+    }
+    
+    public List<Medicamento> findAll() {
+    	return medicamentoRepository.findAll();
     }
 
 }
