4db135b8743350e5d346b26f31868601d7ba2b2f
Gil <eu@anywhere.com>
2018-05-04 17:49:28 -0300

    .

--- src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/model/SolicitacaoExame.java
+++ src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/model/SolicitacaoExame.java
@@ -2,7 +2,11 @@ package com.pucmg.tcc.gcbl.proposta3.clinica.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.NotEmpty;
 
@@ -20,9 +24,16 @@ public class SolicitacaoExame  extends BaseEntity {
     private String dataSolicitacao;
 
 //    private Exame exame;
-//    private Medico medicoSolicitante;
-//    private Paciente paciente;
 
+    @NotNull(message = "*Por favor informe o paciente")
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name="ID_PACIENTE")
+    private Paciente paciente;
+
+    @NotNull(message = "*Por favor informe o medico")
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name="ID_MEDICO")
+    private Medico medicoSolicitante;
     
 }
 
