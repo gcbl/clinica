f853ac89b22a0238e97502218268d749cfe64d71
Gil <eu@anywhere.com>
2018-05-23 19:02:55 -0300

    .

---
+++ src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/service/SolicitacaoExameService.java
@@ -0,0 +1,39 @@
+package com.pucmg.tcc.gcbl.proposta3.clinica.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pucmg.tcc.gcbl.proposta3.clinica.model.SolicitacaoExame;
+import com.pucmg.tcc.gcbl.proposta3.clinica.repository.SolicitacaoExameRepository;
+
+@Service
+public class SolicitacaoExameService{
+
+    @Autowired
+    protected SolicitacaoExameRepository repository;
+    
+    
+    public void salvar(SolicitacaoExame item) {
+        repository.save(item);
+    }
+
+    public void excluir(String id) {
+        repository.delete(id);
+    }
+
+    public boolean exists(String id) {
+        return repository.exists(id);
+    }
+    
+    public SolicitacaoExame findOne(String id) {
+        return repository.findOne(id);
+    }
+    
+    public List<SolicitacaoExame> findAll() {
+        return repository.findAll();
+    }
+
+    
+}
