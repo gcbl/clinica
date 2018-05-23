721b8a2b618e45de2515e09c228b54dbbcada7d5
Gil <eu@anywhere.com>
2018-05-23 15:03:37 -0300

    .

--- src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/service/ExameService.java
+++ src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/service/ExameService.java
@@ -8,15 +8,32 @@ import org.springframework.stereotype.Service;
 import com.pucmg.tcc.gcbl.proposta3.clinica.model.Exame;
 import com.pucmg.tcc.gcbl.proposta3.clinica.repository.ExameRepository;
 
-//@Service("userService")
 @Service
 public class ExameService{
 
-	@Autowired
-	protected ExameRepository exameRepository;
-	
-    public List<Exame> findAll() {
-    	return exameRepository.findAll();
+    @Autowired
+    protected ExameRepository repository;
+    
+    
+    public void salvar(Exame item) {
+        repository.save(item);
     }
 
+    public void excluir(String id) {
+        repository.delete(id);
+    }
+
+    public boolean exists(String id) {
+        return repository.exists(id);
+    }
+    
+    public Exame findOne(String id) {
+        return repository.findOne(id);
+    }
+    
+    public List<Exame> findAll() {
+        return repository.findAll();
+    }
+
+    
 }
