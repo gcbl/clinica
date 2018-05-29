21214f9d406ecefc5a7bf1a6297cb9a8c1df9fff
Gil <Gil@somewhere>
2018-05-28 23:32:15 -0300

    .

---
+++ src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/service/ReceitaService.java
@@ -0,0 +1,38 @@
+package com.pucmg.tcc.gcbl.proposta3.clinica.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pucmg.tcc.gcbl.proposta3.clinica.model.Receita;
+import com.pucmg.tcc.gcbl.proposta3.clinica.repository.ReceitaRepository;
+
+@Service
+public class ReceitaService{
+
+    @Autowired
+    protected ReceitaRepository repository;
+    
+    
+    public void salvar(Receita item) {
+        repository.save(item);
+    }
+
+    public void excluir(String id) {
+        repository.delete(id);
+    }
+
+    public boolean exists(String id) {
+        return repository.exists(id);
+    }
+    
+    public Receita findOne(String id) {
+        return repository.findOne(id);
+    }
+    
+    public List<Receita> findAll() {
+        return repository.findAll();
+    }
+    
+}
