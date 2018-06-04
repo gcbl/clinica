89e0ac7b4084d77e82796598674b94c8ac986898
Gil <gilcblira@gmail.com>
2018-06-04 18:24:23 -0300

    .

---
+++ src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/repository/util/LocalDateAttributeConverter.java
@@ -0,0 +1,21 @@
+package com.pucmg.tcc.gcbl.proposta3.clinica.repository.util;
+
+import java.sql.Date;
+import java.time.LocalDate;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+@Converter(autoApply = true)
+public class LocalDateAttributeConverter implements AttributeConverter<LocalDate, Date> {
+    
+    @Override
+    public Date convertToDatabaseColumn(LocalDate locDate) {
+        return (locDate == null ? null : Date.valueOf(locDate));
+    }
+
+    @Override
+    public LocalDate convertToEntityAttribute(Date sqlDate) {
+        return (sqlDate == null ? null : sqlDate.toLocalDate());
+    }
+}
