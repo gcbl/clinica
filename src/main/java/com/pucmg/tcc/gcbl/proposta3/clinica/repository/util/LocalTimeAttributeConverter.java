b1b7c61850175ee27b0ffedaf8942304d1f0a17e
Gil <gilcblira@gmail.com>
2018-06-05 18:02:39 -0300

    ---

---
+++ src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/repository/util/LocalTimeAttributeConverter.java
@@ -0,0 +1,22 @@
+package com.pucmg.tcc.gcbl.proposta3.clinica.repository.util;
+
+import java.sql.Time;
+import java.time.LocalTime;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+@Converter(autoApply = true)
+public class LocalTimeAttributeConverter implements AttributeConverter<LocalTime, Time> {
+
+    @Override
+    public Time convertToDatabaseColumn(LocalTime localTime) {
+        return (localTime == null ? null : Time.valueOf(localTime));
+    }
+
+    @Override
+    public LocalTime convertToEntityAttribute(Time sqlTime) {
+        return (sqlTime == null ? null : sqlTime.toLocalTime());
+    }
+    
+}
