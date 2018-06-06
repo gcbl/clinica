71fddb5a769445172c2067de745f67991ed88c74
Gil <gilcblira@gmail.com>
2018-06-06 19:30:50 -0300

    ---

--- src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/service/AgendamentoService.java
+++ src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/service/AgendamentoService.java
@@ -1,8 +1,5 @@
 package com.pucmg.tcc.gcbl.proposta3.clinica.service;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,28 +14,6 @@ public class AgendamentoService{
 
     @Autowired
 	private AgendamentoRepository repository;
-
-    public void criarVagas(LocalDate dateInicio, LocalDate dataFim, Medico medico) {
-        List<Agendamento> vagas = new ArrayList<Agendamento>();
-        
-        LocalDate data = dateInicio;
-        while(data.isBefore(dataFim)){
-            Agendamento agendamento = new Agendamento();
-            agendamento.setMedico(medico);
-            agendamento.setData(data);
-            
-            agendamento.setHoraInicio( LocalTime.of(8, 0) );
-            agendamento.setHoraFim( LocalTime.of(9, 0) );
-
-            vagas.add(agendamento);
-            
-            data = data.plusDays(1);
-        }
-        
-        
-        repository.save(vagas);
-        
-    }
 
     public List<Agendamento> getAgendaMedico(Medico medico) {
         return repository.findByMedico(medico);
