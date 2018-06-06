a9f452d36244dc7490bd75acd9636ea076190e13
Gil <gilcblira@gmail.com>
2018-06-06 14:55:45 -0300

    ----

--- src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/model/Pessoa.java
+++ src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/model/Pessoa.java
@@ -12,6 +12,7 @@ import org.hibernate.validator.constraints.NotEmpty;
 import org.hibernate.validator.constraints.br.CPF;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.pucmg.tcc.gcbl.proposta3.clinica.util.StringUtils;
 
 import lombok.Data;
@@ -29,32 +30,39 @@ public abstract class Pessoa extends BaseEntity implements Comparable<Pessoa> {
     //TODO: Ativar o unique=true e tratar quando for inserir um mesmo CPF
     @Column(name = "CPF" /*, unique=true */)
     @CPF(message = "Por favor informe um CPF válido")
+    @JsonIgnore
     private String cpf;
     
     @Column(name = "DT_NASCIMENTO")
     @DateTimeFormat(pattern = "dd/MM/yyyy" )
     @NotNull(message = "Por favor informe a data de nascimento")
     @Past(message = "A data de nascimento deve ser no passado")
+    @JsonIgnore
     private Date dataNascimento;
 
     @Column(name = "ENDERECO")
     //@NotEmpty(message = "*Por favor informe o endereco")
+    @JsonIgnore
     private String endereco;
     
     @Column(name = "BAIRRO")
     //@NotEmpty(message = "*Por favor informe o endereco")
+    @JsonIgnore
     private String bairro;    
     
     @Column(name = "CEP")
     //@NotEmpty(message = "*Por favor informe o CEP")
+    @JsonIgnore
     private String cep;
 
     @Column(name = "CIDADE")
     //@NotEmpty(message = "*Por favor informe a cidade")
+    @JsonIgnore
     private String cidade;
 
     @Column(name = "ESTADO")
     //@NotEmpty(message = "*Por favor informe o estado")
+    @JsonIgnore
     private String estado;
     
     
