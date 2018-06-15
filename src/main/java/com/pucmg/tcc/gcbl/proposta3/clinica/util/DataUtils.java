2abce085315599570835f5ac9373f11c56132001
Gil <gilcblira@gmail.com>
2018-06-15 16:19:17 -0300

    ----

--- src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/util/DataUtils.java
+++ src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/util/DataUtils.java
@@ -1,11 +1,14 @@
 package com.pucmg.tcc.gcbl.proposta3.clinica.util;
 
 import java.text.DecimalFormat;
+import java.time.DayOfWeek;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class DataUtils {
 
@@ -25,10 +28,50 @@ public class DataUtils {
       return Instant.ofEpochMilli(date.getTime()).atZone(ZoneId.systemDefault()).toLocalDateTime();
     }
     
+    public static boolean isWeekend(Date data){
+        Calendar cal = new GregorianCalendar();
+        cal.setTime(data);
+        int day = cal.get(Calendar.DAY_OF_WEEK);
+        return day == Calendar.SUNDAY || day == Calendar.SATURDAY;
+    }
     
-    private static final DecimalFormat CPF_NFORMAT = new DecimalFormat("00000000000");
+    public static boolean isSaturday(Date data){
+        Calendar cal = new GregorianCalendar();
+        cal.setTime(data);
+        int day = cal.get(Calendar.DAY_OF_WEEK);
+        return day == Calendar.SATURDAY;
+    }
+    
+    public static boolean isSunday(Date data){
+        Calendar cal = new GregorianCalendar();
+        cal.setTime(data);
+        int day = cal.get(Calendar.DAY_OF_WEEK);
+        return day == Calendar.SUNDAY;
+    }    
+    
+    ///
+    public static boolean isWeekend(LocalDate data){
+        DayOfWeek diaDaSemana = data.getDayOfWeek();
+        return diaDaSemana == DayOfWeek.SUNDAY || diaDaSemana == DayOfWeek.SATURDAY;
+    }
+    
+    public static boolean isSaturday(LocalDate data){
+        DayOfWeek diaDaSemana = data.getDayOfWeek();
+        return diaDaSemana == DayOfWeek.SATURDAY;
+    }
+    
+    public static boolean isSunday(LocalDate data){
+        DayOfWeek diaDaSemana = data.getDayOfWeek();
+        return diaDaSemana == DayOfWeek.SUNDAY;
+    }   
+    ///
+    
+    
+    
+    
+    private static final DecimalFormat CPF_FORMAT = new DecimalFormat("00000000000");
     public String getCpfFormatado(long cpf){
-        final String stringNumber = CPF_NFORMAT.format(cpf);
+        final String stringNumber = CPF_FORMAT.format(cpf);
         return stringNumber.replaceAll("([0-9]{3})([0-9]{3})([0-9]{3})([0-9]{2})", "$1.$2.$3-$4");   
     }
 
