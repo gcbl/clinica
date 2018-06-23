4cf244c67560fb3db0b96335c337187b4b566da4
Gil <gilcblira@gmail.com>
2018-06-23 16:30:58 -0300

    ---

--- src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/model/BaseEntity.java
+++ src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/model/BaseEntity.java
@@ -1,14 +1,10 @@
 package com.pucmg.tcc.gcbl.proposta3.clinica.model;
 
-import java.util.Date;
-
 import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Data;
 
@@ -21,8 +17,8 @@ public class BaseEntity {
     @Column(name = "ID")
     private String id;
     
-    @JsonIgnore
-    @Column(name = "DT_CRIACAO")
-    private Date dataCriacao = new Date();
+//    @JsonIgnore
+//    @Column(name = "DT_CRIACAO")
+//    private Date dataCriacao = new Date();
 
 }
