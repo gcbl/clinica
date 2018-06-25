534d3ee618b00be9d7284da4ef9e7a7c5dde35b3
Gil <gilcblira@gmail.com>
2018-06-25 15:29:26 -0300

    ---

--- src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/repository/ReceitaRepository.java
+++ src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/repository/ReceitaRepository.java
@@ -1,11 +1,16 @@
 package com.pucmg.tcc.gcbl.proposta3.clinica.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.pucmg.tcc.gcbl.proposta3.clinica.model.Receita;
+import com.pucmg.tcc.gcbl.proposta3.clinica.model.Paciente;
 
 @Repository
 public interface ReceitaRepository extends JpaRepository<Receita, String> {
 
+    public List<Receita> findByPaciente(Paciente paciente);
+    
 }
