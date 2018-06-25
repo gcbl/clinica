f86c1da8a34f84f29e2dac69e2586d2fff0acf24
Gil <gilcblira@gmail.com>
2018-06-25 15:28:18 -0300

    ---

--- src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/model/BaseEntity.java
+++ src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/model/BaseEntity.java
@@ -1,10 +1,16 @@
 package com.pucmg.tcc.gcbl.proposta3.clinica.model;
 
+import java.time.LocalDateTime;
+
 import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Data;
 
@@ -17,8 +23,26 @@ public class BaseEntity {
     @Column(name = "ID")
     private String id;
     
-//    @JsonIgnore
-//    @Column(name = "DT_CRIACAO")
-//    private Date dataCriacao = new Date();
+    @JsonIgnore
+    @Column(name = "DT_CRIACAO")
+    private LocalDateTime dataCriacao;
+
+    @JsonIgnore
+    @Column(name = "DT_ULTIMA_ATUALIZACAO")
+    private LocalDateTime dataAtualizacao;
+    
+    
+    @PrePersist
+    public void prePersist() {
+        dataCriacao = LocalDateTime.now();
+        //createdBy = LoggedUser.get();
+    }
+    
+    @PreUpdate
+    public void preUpdate() {
+        dataAtualizacao = LocalDateTime.now();
+        //createdBy = LoggedUser.get();
+    }
+    
 
 }
