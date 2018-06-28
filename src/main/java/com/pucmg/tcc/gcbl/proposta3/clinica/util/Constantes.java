95369da8c6b3b9b7773858deedf64b1efb82eb55
Gil <gilcblira@gmail.com>
2018-06-28 17:30:59 -0300

    ---

--- src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/util/Constantes.java
+++ src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/util/Constantes.java
@@ -24,6 +24,7 @@ public class Constantes {
     public static final String ACAO_INCLUIR = "incluir";
     public static final String ACAO_EXCLUIR = "excluir";
     public static final String ACAO_EDITAR = "editar";
+    public static final String ACAO_PESQUISAR = "pesquisar";
     
 
     // API Ajax search
