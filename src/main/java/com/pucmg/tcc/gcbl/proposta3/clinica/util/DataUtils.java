1e14a9cbd12e9400456114d7ee2747c80f06cbaa
Gil <gilcblira@gmail.com>
2018-06-28 15:21:29 -0300

    ---

--- src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/util/DataUtils.java
+++ src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/util/DataUtils.java
@@ -64,11 +64,62 @@ public class DataUtils {
         DayOfWeek diaDaSemana = data.getDayOfWeek();
         return diaDaSemana == DayOfWeek.SUNDAY;
     }   
-    ///
+
+    public static String calculaIdadeAnoMesDia(Date dataNascimento) {
+        Calendar calHoje = GregorianCalendar.getInstance();
+        int diaHoje = calHoje.get(Calendar.DAY_OF_MONTH);
+        int mesHoje = calHoje.get(Calendar.MONTH) + 1;
+        int anoHoje = calHoje.get(Calendar.YEAR);
+        
+        // Data do nascimento
+        Calendar calNiver = Calendar.getInstance();
+        calNiver.setTime(dataNascimento);
+        int diaAniversario = calNiver.get(Calendar.DAY_OF_MONTH);
+        int mesAniversario = calNiver.get(Calendar.MONTH) + 1;
+        int anoAniversario = calNiver.get(Calendar.YEAR);        
+        
+        int anos = (calHoje.getTimeInMillis() < calNiver.getTimeInMillis()) ? (anoHoje - anoAniversario - 1) : (anoHoje - anoAniversario);
+        int meses;
+        int dias;
+        
+        meses = mesHoje - mesAniversario;
+        if (meses > 0) { //Verificando se já fez aniversário ou não
+            if (diaHoje < diaAniversario) {
+                meses--;
+            }
+        } else if (meses < 0) { //Se o mês atual for menor que o mês do aniversário
+            meses = 12 + meses; //Lembrar que meses está negativo, por isso a soma;
+            //Da mesma forma, vamos comparar o dia atual com o dia do aniversário, para sabermos se o mês está completo ou não:
+            if (diaHoje < diaAniversario) {
+                meses--;
+            }
+        } else {//Se o mês atual for o mês do aniversário:
+            if (diaHoje<diaAniversario) {
+                meses = 11;
+            } 
+        }
+        
+        dias = diaHoje - diaAniversario;
+        if (dias < 0) {// Se dia hoje menor que dia do niver, somar os dias desde o mês anterior:
+            if (mesHoje == 5 || mesHoje == 7 || mesHoje == 8 || mesHoje == 10 || mesHoje == 12) {
+                dias = 30 - diaAniversario + diaHoje;
+            } else if (mesHoje == 1 || mesHoje == 2 || mesHoje == 4 || mesHoje == 6 || mesHoje == 9 || mesHoje == 11) {
+                dias = 31 - diaAniversario + diaHoje;
+            } else {// Verificando se o ano é bissexto ou não: Esse else é para
+                    // o mês 3, cujo anterior é fevereiro:
+                if (anoHoje % 4 == 0) {
+                    dias = 29 - diaAniversario + diaHoje;
+                } else {
+                    dias = 28 - diaAniversario + diaHoje;
+                }
+            }
+        }
+        
+        System.out.println(anos + "a " + meses + "m " + dias + "d");
+        return anos + " anos, " + meses + " meses e " + dias + " dias";
+    }    
     
-    
-    
-    
+    // -------
     private static final DecimalFormat CPF_FORMAT = new DecimalFormat("00000000000");
     public String getCpfFormatado(long cpf){
         final String stringNumber = CPF_FORMAT.format(cpf);
