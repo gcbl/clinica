a4d2bb434babec3013a5278663fc91b52232bfd1
Gil <gilcblira@gmail.com>
2018-06-29 17:15:52 -0300

    ---

--- src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/service/ResultadoExameService.java
+++ src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/service/ResultadoExameService.java
@@ -5,18 +5,18 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.pucmg.tcc.gcbl.proposta3.clinica.model.Receita;
 import com.pucmg.tcc.gcbl.proposta3.clinica.model.Paciente;
-import com.pucmg.tcc.gcbl.proposta3.clinica.repository.ReceitaRepository;
+import com.pucmg.tcc.gcbl.proposta3.clinica.model.ResultadoExame;
+import com.pucmg.tcc.gcbl.proposta3.clinica.repository.ResultadoExameRepository;
 
 @Service
 public class ResultadoExameService{
 
     @Autowired
-    protected ReceitaRepository repository;
+    protected ResultadoExameRepository repository;
     
     
-    public void salvar(Receita item) {
+    public void salvar(ResultadoExame item) {
         repository.save(item);
     }
 
@@ -28,15 +28,15 @@ public class ResultadoExameService{
         return repository.exists(id);
     }
     
-    public Receita findOne(String id) {
+    public ResultadoExame findOne(String id) {
         return repository.findOne(id);
     }
     
-    public List<Receita> findAll() {
+    public List<ResultadoExame> findAll() {
         return repository.findAll();
     }
     
-    public List<Receita> findByPaciente(Paciente paciente) {
+    public List<ResultadoExame> findByPaciente(Paciente paciente) {
         return repository.findByPaciente(paciente);
     }    
     
