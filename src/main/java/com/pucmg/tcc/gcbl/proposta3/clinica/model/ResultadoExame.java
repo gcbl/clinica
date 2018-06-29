98ca6b654e8271ab25a1b9ab7688314cf72eb7dd
Gil <gilcblira@gmail.com>
2018-06-29 17:34:12 -0300

    ---

--- src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/model/ResultadoExame.java
+++ src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/model/ResultadoExame.java
@@ -1,6 +1,11 @@
 package com.pucmg.tcc.gcbl.proposta3.clinica.model;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.NotEmpty;
@@ -10,9 +15,14 @@ import lombok.EqualsAndHashCode;
 
 @Data
 @EqualsAndHashCode(callSuper = false)
-//@Entity
-//@Table(name = "SC_RESULTADO_EXAME")
+@Entity
+@Table(name = "SC_RESULTADO_EXAME")
 public class ResultadoExame extends BaseEntity {
+    
+    @NotNull(message = "*Por favor informe o paciente")
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name="ID_PACIENTE")
+    private Paciente paciente;    
 
     @Column(name = "LOB_ANEXOS")
     private byte[] anexos;
@@ -22,7 +32,7 @@ public class ResultadoExame extends BaseEntity {
     private String resultado;
 
     @NotNull(message = "Por favor informe se o resultado deve ser disponibilizado ao paciente")
-    @Column(name = "BL_DISPONIBILIZADO")
+    @Column(name = "IS_DISPONIBILIZADO")
     private boolean disponibilizadoPaciente;
     
 //    @OneToOne(mappedBy="resultadoExame")
