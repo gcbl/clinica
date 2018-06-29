5cff8ba7784b2bf934f215fc843f2063ae7eb72e
Gil <gilcblira@gmail.com>
2018-06-29 17:01:37 -0300

    ---

--- src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/model/ResultadoExame.java
+++ src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/model/ResultadoExame.java
@@ -1,27 +1,31 @@
 package com.pucmg.tcc.gcbl.proposta3.clinica.model;
 
 import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.NotEmpty;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
 @EqualsAndHashCode(callSuper = false)
-@Entity
-@Table(name = "SC_RESULTADO_EXAME")
+//@Entity
+//@Table(name = "SC_RESULTADO_EXAME")
 public class ResultadoExame extends BaseEntity {
-
-
-    @Column(name = "TX_RESULTADO")
-    private String resultado;
 
     @Column(name = "LOB_ANEXOS")
     private byte[] anexos;
 
-    @Column(name = "BL_PUBLICADO")
-    private boolean publicado;
+    @NotEmpty(message = "Por favor informe o resultado")
+    @Column(name = "TX_RESULTADO")
+    private String resultado;
+
+    @NotNull(message = "Por favor informe se o resultado deve ser disponibilizado ao paciente")
+    @Column(name = "BL_DISPONIBILIZADO")
+    private boolean disponibilizadoPaciente;
     
+//    @OneToOne(mappedBy="resultadoExame")
+//    private SolicitacaoExame solicitacaoExame;
     
 }
