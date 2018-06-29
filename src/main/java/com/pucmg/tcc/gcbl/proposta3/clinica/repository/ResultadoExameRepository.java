b1625b72a2cc4b12513934806f31050857332000
Gil <gilcblira@gmail.com>
2018-06-29 17:10:13 -0300

    ---

--- src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/repository/ResultadoExameRepository.java
+++ src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/repository/ResultadoExameRepository.java
@@ -1,12 +1,16 @@
 package com.pucmg.tcc.gcbl.proposta3.clinica.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.pucmg.tcc.gcbl.proposta3.clinica.model.Paciente;
 import com.pucmg.tcc.gcbl.proposta3.clinica.model.ResultadoExame;
 
 @Repository
 public interface ResultadoExameRepository extends JpaRepository<ResultadoExame, String> {
 
-
+    public List<ResultadoExame> findByPaciente(Paciente paciente);
+    
 }
