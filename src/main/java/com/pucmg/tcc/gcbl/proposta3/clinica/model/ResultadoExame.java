2eb1aeaad503f7e979c448b456e255c6fc6a9b9f
gilvandro <gilvandro@SASIS-GILVANDRO.tre-df.gov.br>
2018-07-02 09:34:37 -0300

    ---

--- src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/model/ResultadoExame.java
+++ src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/model/ResultadoExame.java
@@ -1,6 +1,7 @@
 package com.pucmg.tcc.gcbl.proposta3.clinica.model;
 
 import java.time.LocalDateTime;
+import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -11,6 +12,7 @@ import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -32,6 +34,19 @@ public class ResultadoExame extends BaseEntity implements HistoricoClinico {
     @JoinColumn(name="ID_PACIENTE")
     private Paciente paciente;    
 
+    //@NotEmpty(message = "Por favor informe nome do laboratório")
+    @Column(name = "TX_LABORATORIO")
+    private String laboratorio;
+
+    //@NotEmpty(message = "Por favor informe nome do responsável técnico")
+    @Column(name = "TX_RESPONSAVEL_TECNICO")
+    private String responsavelTecnico;
+
+    @Column(name = "DT_EXAME")
+    @DateTimeFormat(pattern = "dd/MM/yyyy" )
+    //@NotNull(message = "*Por favor informe a data que o exame foi feito")
+    private Date dataExame;
+    
     @Column(name = "LOB_ANEXOS")
     private byte[] anexos;
 
