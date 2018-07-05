caad679bb6fa99c12fc3a94d48285a759bd6bc3a
Gil <gilcblira@gmail.com>
2018-07-05 18:29:13 -0300

    ---

--- src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/configuration/SecurityConfigurationNovo.java
+++ src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/configuration/SecurityConfigurationNovo.java
@@ -5,9 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.encoding.PlaintextPasswordEncoder;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import com.pucmg.tcc.gcbl.proposta3.clinica.model.security.CustomUserDetailsService;
 
@@ -30,12 +32,42 @@ public class SecurityConfigurationNovo extends WebSecurityConfigurerAdapter {
     }  
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-      http
-          .authorizeRequests()
-              .anyRequest().authenticated()
-          .and()
-          .formLogin(); // <<< de HTTP Basic para formulário
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+            .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated()
+            .and()
+            //.csrf().disable()
+            .formLogin()
+                .loginPage("/login").permitAll()
+                .defaultSuccessUrl("/privado/inicio")
+            .and().
+            logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/")
+            .and()
+                .exceptionHandling()
+                .accessDeniedPage("/access-denied")
+            .and()
+            .rememberMe(); // <<< Habilita a função de "lembrar-me".        
+        
+            configurarAutorizacoes(httpSecurity);
+    }
+
+    private void configurarAutorizacoes(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity.
+            authorizeRequests()
+                .antMatchers("/vendas/relatorios/equipe").hasRole("VISUALIZAR_RELATORIO_EQUIPE")
+                .antMatchers("/vendas/relatorios/custos").hasRole("VISUALIZAR_RELATORIO_CUSTOS");
+        
+    }    
+    
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+           .ignoring()
+           .antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**", "/console/**");
     }    
     
     public static void main(String[] args) {
