a54e03d79213c09d5ff21e2cabcd2e40a64de986
Gil <gilcblira@gmail.com>
2018-07-09 19:44:38 -0300

    ---

--- src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/model/Medico.java
+++ src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/model/Medico.java
@@ -6,6 +6,8 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 import javax.validation.constraints.Digits;
 
+import org.hibernate.validator.constraints.NotEmpty;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Data;
@@ -15,7 +17,7 @@ import com.pucmg.tcc.gcbl.proposta3.clinica.model.security.Usuario;
 
 @Data
 @EqualsAndHashCode(callSuper = false)
-@ToString(callSuper = true)
+//@ToString(callSuper = true)
 @Entity
 @Table(name = "SC_MEDICO")
 public class Medico extends Usuario {
@@ -24,7 +26,8 @@ public class Medico extends Usuario {
     // TODO: Ativar o unique=true e tratar quando for inserir um medico com o mesmo CRM
     @Column(name = "CRM", 
             nullable = false /*, unique=true */) // unique=true pra criar a constraint ao criar o banco sozinho. 
-    @Digits(fraction=0, integer=10, message = "Por favor informe o número do CRM")
+    @Digits(fraction=0, integer=10, message = "O CRM deve ser um número")
+	@NotEmpty(message = "*Por favor informe o CRM do médico")
     private String crm;
 
 	@JsonIgnore
