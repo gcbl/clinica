bb2f7469d35e25276c045ff3db36da353e6fe2ee
Gil <gilcblira@gmail.com>
2018-07-08 23:13:48 -0300

    ---

--- src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/repository/security/GrupoRepossitory.java
+++ src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/repository/security/GrupoRepossitory.java
@@ -1,15 +1,12 @@
 package com.pucmg.tcc.gcbl.proposta3.clinica.repository.security;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.pucmg.tcc.gcbl.proposta3.clinica.model.security.Grupo;
-import com.pucmg.tcc.gcbl.proposta3.clinica.model.security.Usuario;
 
 
 public interface GrupoRepossitory extends JpaRepository<Grupo, Long> {
     
-    List<Grupo> findByUsuariosIn(Usuario usuario);
+    //List<Grupo> findByUsuariosIn(Usuario usuario);
 
 }
--- src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/repository/security/PermissaoRepository.java
+++ src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/repository/security/PermissaoRepository.java
@@ -1,15 +1,12 @@
 package com.pucmg.tcc.gcbl.proposta3.clinica.repository.security;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.pucmg.tcc.gcbl.proposta3.clinica.model.security.Grupo;
 import com.pucmg.tcc.gcbl.proposta3.clinica.model.security.Permissao;
 
 
 public interface PermissaoRepository extends JpaRepository<Permissao, Long> {
     
-    List<Permissao> findByGruposIn(Grupo grupo);
+    //List<Permissao> findByGruposIn(Grupo grupo);
 
 }
