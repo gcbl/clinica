00884a6b4e1b73b6cdce216f68dc7ec52e27a4ca
Gil <gilcblira@gmail.com>
2018-07-09 14:56:46 -0300

    ---

--- src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/repository/security/GrupoRepository.java
+++ src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/repository/security/GrupoRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.pucmg.tcc.gcbl.proposta3.clinica.model.security.Grupo;
 
 
-public interface GrupoRepossitory extends JpaRepository<Grupo, Long> {
+public interface GrupoRepository extends JpaRepository<Grupo, Long> {
     
     //List<Grupo> findByUsuariosIn(Usuario usuario);
 
