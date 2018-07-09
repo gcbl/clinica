f8fb19b9d2365b9f573b43a17d1be2b6ab5d9a29
Gil <gilcblira@gmail.com>
2018-07-09 14:54:22 -0300

    ---

--- src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/model/security/Usuario.java
+++ src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/model/security/Usuario.java
@@ -4,12 +4,15 @@ import java.io.Serializable;
 import java.util.List;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+
+import org.hibernate.validator.constraints.NotEmpty;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.pucmg.tcc.gcbl.proposta3.clinica.model.Pessoa;
@@ -28,7 +31,12 @@ public class Usuario extends Pessoa implements Serializable {
 
     private static final long serialVersionUID = 1L;
     
+    @Column(name = "LOGIN", nullable=false, unique=true)
+    @NotEmpty(message = "*Por favor informe o login")
     private String login;
+    
+    @Column(name = "SENHA", nullable=false, unique=false)
+    @NotEmpty(message = "*Por favor informe a senha")
     private String senha;
 //    private boolean ativo;
     
