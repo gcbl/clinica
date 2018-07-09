fecd08c6bd39f5c88f815613a9d59da3dc0db844
Gil <gilcblira@gmail.com>
2018-07-09 00:53:30 -0300

    ---

--- src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/model/security/UsuarioSistema.java
+++ src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/model/security/UsuarioSistema.java
@@ -5,19 +5,23 @@ import java.util.Collection;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 
+import lombok.Data;
+
+@Data
 public class UsuarioSistema extends User {
 
     private static final long serialVersionUID = 1L;
     
-    private String nome;
+    private Usuario usuario;
     
-    public UsuarioSistema(String nome, String username, String password, Collection<? extends GrantedAuthority> authorities) {
+    
+    public UsuarioSistema(Usuario usuario, String username, String password, Collection<? extends GrantedAuthority> authorities) {
         super(username, password, authorities);
         
-        this.nome = nome;
+        this.usuario = usuario;
     }
     
     public String getNome() {
-        return nome;
+        return getUsuario().getNome();
     }
 }
