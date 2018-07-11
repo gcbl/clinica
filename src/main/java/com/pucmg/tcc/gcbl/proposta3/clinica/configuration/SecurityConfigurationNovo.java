dc19181caa9562cf75e9a8cdd2757b7dae36090c
Gil <gilcblira@gmail.com>
2018-07-11 19:28:47 -0300

    ---

--- src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/configuration/SecurityConfigurationNovo.java
+++ src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/configuration/SecurityConfigurationNovo.java
@@ -159,7 +159,7 @@ public class SecurityConfigurationNovo extends WebSecurityConfigurerAdapter {
     }    
     
     public static void main(String[] args) {
-        System.out.println(new BCryptPasswordEncoder().encode("teste"));
+        System.out.println(new BCryptPasswordEncoder().encode("1"));
     }    
     
 }
