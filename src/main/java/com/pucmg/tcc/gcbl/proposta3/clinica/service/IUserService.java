fa08b6177842d773d6d2e862555aca30478640a9
Gil <gilcblira@gmail.com>
2018-07-12 17:50:38 -0300

    removendo autenticacao antiga

--- src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/service/IUserService.java
+++
@@ -1,9 +0,0 @@
-package com.pucmg.tcc.gcbl.proposta3.clinica.service;
-
-
-import com.pucmg.tcc.gcbl.proposta3.clinica.model.User;
-
-public interface IUserService {
-    public User findUserByEmail(String email);
-    public void saveUser(User user);
-}
