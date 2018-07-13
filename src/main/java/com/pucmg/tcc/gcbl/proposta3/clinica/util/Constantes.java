244cc8e389afb8f219f862694b8e7e2d2cfd5b09
Gil <gilcblira@gmail.com>
2018-07-13 19:20:56 -0300

    ---

--- src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/util/Constantes.java
+++ src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/util/Constantes.java
@@ -2,7 +2,7 @@ package com.pucmg.tcc.gcbl.proposta3.clinica.util;
 
 public class Constantes {
  
-    public static final String VERSAO_DO_SISTEMA = "201804181819";  // Versao do sistema
+    public static final String VERSAO_DO_SISTEMA = "201807131913"; // "201804181819";  // Versao do sistema
 
     
     // Constantes de alerta
