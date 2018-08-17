6e4481a4c020ab9cc56af86a0556a330bdb6cb28
Gil <gilcblira@gmail.com>
2018-08-17 14:45:22 -0300

    ---

--- src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/controller/GrupoController.java
+++ src/main/java/com/pucmg/tcc/gcbl/proposta3/clinica/controller/GrupoController.java
@@ -146,6 +146,10 @@ public class GrupoController extends ModelController {
         if(result.hasErrors()){
             model.addAttribute(getModelName(), item);
             
+            List<Permissao> permissoesList = permissaoService.findAll();
+            Collections.sort(permissoesList);
+            model.addAttribute("permissoesList", permissoesList);
+            
             mensagem = messageSource.getMessage("formulario.erros-de-validacao", null, locale);
             adicionarAlertaWarning(model, mensagem);
             return getViewPath() + "incluirForm";
